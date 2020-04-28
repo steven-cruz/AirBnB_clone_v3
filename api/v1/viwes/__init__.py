0cab28ed20233960f3f4ee8d6ce984a79209d2b1
JuanDavidDava2 <juandm417@gmail.com>
2020-04-28 21:15:05 +0000

    delete folder fail

--- api/v1/viwes/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-""" Blueprint - variable name: app_views """
-from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-if app_views is not None:
-    from api.v1.views.index import *
