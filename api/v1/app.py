43564471713ef8f260ca4bc4985d195720168f26
steven-cruz <stevencruz.300008@gmail.com>
2020-04-28 18:37:22 +0000

    Python api built with Flask

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Python api bulit with Flask """
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """ Calls storage close on appcontext """
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=int(port), threaded=True)
