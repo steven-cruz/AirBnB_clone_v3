57ac775b552e707ee477faaf14352706bc2cf3c4
steven-cruz <stevencruz.300008@gmail.com>
2020-04-29 21:22:49 +0000

    upgrade of states file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,36 +1,52 @@
 #!/usr/bin/python3
 """ module state viwes"""
-
-
 from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.state import State
-app.url_map.strict_slashes = False
 
 
-@app_views.route('/states/',methods=['GET'])
-def all_states():
-    """Retrieves the list of all State objects"""
-    all_states = []
-    for state in storage.all('State').values():
-        all_states.append(state.to_dict())
-    return jsonify(all_states)
-
-
-@app_views.route('/states/<state_id>', method=['GET'])
-def retrieve_state(state_id):
-    """If the state_id is not linked to any State object, raise a 404 error """
+ @app_views.route('/states/', methods=['GET'])
+@app_views.route('/states/<state_id>', methods=['GET'])
+def all_states(state_id=None):
+    '''Returns all states object in json format'''
+    json_list = []
     try:
-        state = jsonify(storage.get('State', state_id).to_dict())
-        return state
-    except:
+        if state_id is None:
+            for v in storage.all('State').values():
+                json_list.append(v.to_dict())
+        else:
+            json_list = storage.get('State', state_id).to_dict()
+        return jsonify(json_list)
+    except Exception:
         abort(404)
 
 
-@app_views.route('/states/<state_id>', method=['DELETE'])
+def attrib_update(obj, **args):
+    '''Helper function to update objects attributes to correct types'''
+    for key, value in args.items():
+        if key not in ['id', 'created_at', 'updated_at'] and hasattr(obj, key):
+            if isinstance(value, str):
+                value = value.replace("_", " ")
+            setattr(obj, key, value)
+
+
+@app_views.route('/states/', methods=['POST'])
+def create_state():
+    '''Creates an instance of State and save it to storage'''
+    form = request.get_json(force=True)
+    if 'name' not in request.json:
+        return jsonify({"error": "Missing name"}), 400
+    state_class = models.classes['State']
+    new_state = state_class()
+    attrib_update(new_state, **form)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
-    """delete state object"""
+    '''Deletes a state object'''
     state_obj = storage.get('State', state_id)
     if state_obj is None:
         abort(404)
@@ -40,26 +56,13 @@ def delete_state(state_id):
     return jsonify({})
 
 
-@app_views.route('/states/', methods=['POST'])
-def create_state():
-    """Create an stance of state and save in to storage"""
-    form = request.get_json(force=True)
-    if 'name' not in request.json:
-        return jsonify({"error: Missing name"}), 400
-    state_class = models.classes['State']
-    new_state = state_class()
-    attrib_update(new_state, **form)
-    new_state.save()
-    return jsonify(new_state.to_dict()), 201
-
-
-@app_niews.route('/states/<state_id>', methods=['PUT'])
+@app_views.route('/states/<state_id>', methods=['PUT'])
 def update_state(state_id):
-    """ Updates State object attribite """
+    '''Updates State object attribute'''
     state_obj = storage.get('State', state_id)
     if state_obj is None:
-            abort(404)
+        abort(404)
     form = request.get_json(force=True)
-    attrib_update(state_obj), **form)
+    attrib_update(state_obj, **form)
     state_obj.save()
     return jsonify(state_obj.to_dict()), 200
