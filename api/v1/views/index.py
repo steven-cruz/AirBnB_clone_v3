43a26ffea50b237a8ab3d0a0b6187a480343bbd6
JuanDavidDava2 <juandm417@gmail.com>
2020-04-30 02:32:55 +0000

    add reviwer

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -18,6 +18,7 @@ def stats():
              "amenities": storage.count("Amenity"),
              "cities": storage.count("City"),
              "places": storage.count("Place"),
+             "reviews": storage.count("Review"),
              "states": storage.count("State"),
              "users": storage.count("User")
     }
